58f8b98715fdd060029d8ee847e2cea373d1032e
rena02to <rena02to@gmail.com>
2023-11-01 23:22:13 -0300

    Deixando só a estrutura necessária para o projeto.

---
+++ components/Navbar.js
@@ -0,0 +1,9 @@
+import style from '../styles/css/Navbar.module.css';
+
+export default function Navbar(){
+    return(
+        <nav className={style.navbar}>
+            <p>Teste</p>
+        </nav>
+    )
+}
